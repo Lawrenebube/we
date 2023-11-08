802bfa6ffdb21200b2a038414768ca6b40ac390a
unknown <lawrenciaebube@gmail.com>
2023-11-08 16:30:57 +0100

    jusew

---
+++ src/views/HomeView.vue
@@ -0,0 +1,452 @@
+<script setup></script>
+
+<template>
+  <div class="fixed">
+    <div class="fixedlogos">
+      <img src="../assets/noun-quota-4750811.svg" alt="">
+      <img src="../assets/2931155_add_attach_create_maximize_new_icon.png" alt="">
+    </div>
+    <div>
+      <div class="fixedLinks">
+        <ul>
+          <h3>ATLAS</h3>
+          <a href="">DINING</a>
+          <a href="">TRAVEL</a>
+          <a href="">CARD</a>
+          <a href="">APP</a>
+          <a href="">CONCIERGE</a>
+        </ul>
+        <ul>
+          <h3>BENEFITS</h3>
+          <a href="">ATLAS BENEFITS</a>
+          <a href="">VISA INFINITE</a>
+        </ul>
+        <ul>
+          <h3>CONTACT</h3>
+          <a href="mailto:">EMAIL</a>
+          <a href="">TWITTER</a>
+          <a href="">INSTAGRAM</a>
+        </ul>
+        <button> BECOMING A FOUNDING MEMBER </button>
+      </div>
+      <!-- <p> 2023 &copy; ATLAS EXPLORATION, INC</p> -->
+    </div>
+  </div>
+  <main>
+    <section class="hero">
+      <h1>No Limits</h1>
+      <video autoplay muted>
+        <source src="../assets/atlas-card.webm" type="video/webm" />
+      </video>
+      <article>
+        This is your friend who scores the hottest tables in town, your passport to weekend
+        getaways, and your guest pass to must-see events. All brilliantly brought together in a card
+        and app of uncompromising beauty and utility. This is the beginning of a journey to a world
+        with no limits.
+      </article>
+      <p>This is Atlas.</p>
+    </section>
+    <div class="blackSection1">
+      <section class="section1">
+        <h2>Taste Maker</h2>
+        <h3>Meet Atlas Supper Club.</h3>
+        <p>
+          Skip past the endless waitlists and concierge calls that turn up empty handed. With Atlas,
+          you have a seat at the table at today's most desirable restaurants.
+        </p>
+        <img src="../assets/images/horsecard3.JPG" alt="a red horse with wings" />
+      </section>
+      <section class="writeblock">
+        <aside>Dedicated reservations¹</aside>
+        <p>
+          Access exclusive inventory at our restaurant partners including – Horses, Felix Trattoria,
+          Cucina Alba, and more.
+        </p>
+      </section>
+      <section class="writeblock">
+        <aside>Last-minute bookings²</aside>
+        <p>With bookings available at short notice, you won't find yourself on a waitlist.</p>
+      </section>
+      <section class="writeblock">
+        <aside>Access during peak hours</aside>
+        <p>Members get seated at prime time, when availability is typically the hardest to get.</p>
+      </section>
+      <div class="images3">
+        <div class="bottle">
+          <img src="../assets/images/horses.JPG"
+            alt="a served table with a plate of noodles with a knife and fork, an empty class cup and an opened bottle" />
+          <p>HORSES, LA, ON ATLAS SUPPER CLUB.</p>
+        </div>
+        <div>
+          <img src="../assets/images/felix.jpg" alt="" />
+          <p>FELIX TRATTORIA, LA, ON ATLAS SUPPER CLUB.</p>
+        </div>
+        <div>
+          <img src="../assets/images/cucina.jpg" alt="" />
+          <p>CUCINA ALBA, NYC, ON ATLAS SUPPER CLUB.</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="neutralSection">
+      <h2>High Flyer</h2>
+      <h3>Built for (spontaneous) travel.</h3>
+      <article>
+        No matter where your next grand tour takes you, Atlas is there as your co-pilot, with
+        bespoke flight benefits for when you're ready to go on the spur of the moment.
+      </article>
+      <div class="threecards">
+        <picture>
+          <source media="(min-width:650px)" srcset="../assets/images/flexcard.JPG" />
+          <source media="(min-width:465px)" srcset="../assets/images/flexcard.JPG" />
+          <img src="../assets/images/flexcard.JPG" alt="Description of the image" />
+        </picture>
+      </div>
+      <p class="bespoke">BESPOKE FLIGHT BENEFITS, INSIDE ATLAS APP.</p>
+
+      <section class="writeblock">
+        <aside>5x points with BLADE³</aside>
+        <p>Exclusive reward for Atlas cardholders on all BLADE helicopter rides and flights.</p>
+      </section>
+      <section class="writeblock">
+        <aside>CLEAR Plus membership⁴</aside>
+        <p>Fast forward through airport security with included CLEAR Plus annual membership.</p>
+      </section>
+      <section class="writeblock">
+        <aside>Cancel flights without fees⁵</aside>
+        <p>
+          Change or cancel any flight, at any time, for any reason and get fees reimbursed up to
+          $600 a year.
+        </p>
+      </section>
+      <section class="visa">
+        <p class="plus">+</p>
+        <h3>Visa Infinite Benefits⁶</h3>
+        <p class="visaP">
+          Priority Pass, Luxury Hotel Collection, no foreign transaction fees, and more.
+        </p>
+        <button>Discover</button>
+      </section>
+      <section class="womanContainer">
+        <div class="woman">
+          <video autoplay muted>
+            <source src="../assets/atlas-woman.mp4" type="video/webm" />
+          </video>
+        </div>
+      </section>
+      <p class="bespoke world">THE WORLD IN YOUR HAND, WITH ATLAS CARD.</p>
+
+      <section>
+        <h2>Head Turner</h2>
+        <h3>Designed to shatter the norm.</h3>
+        <article>
+          Atlas is crafted to mirror our uncommon obsession with every single detail, always in
+          pursuit of elevating you above the crowd, while also enhancing your privacy.
+        </article>
+      </section>
+      <section class="atlascards">
+        <div class="cardFront">
+          <img src="../assets/Capture7.JPG" alt="" />
+        </div>
+        <div class="cardBack">
+          <!-- <img src="../assets/images/atlasbrightcard.JPG" alt=""> -->
+          <img src="../assets/Capture6.JPG" alt="" />
+        </div>
+      </section>
+      <p class="bespoke fronttoback">DESIGNED AND ENGINEERED, FRONT TO BACK.</p>
+      <div class="more">
+        <section class="writeblock">
+          <aside>Advanced construction</aside>
+          <p>
+            Made from 21 grams of high density steel, CNC-milled and engraved, then polished to a
+            brilliant mirror finish that is unlike any card.
+          </p>
+        </section>
+        <section class="writeblock">
+          <aside>Enhanced privacy</aside>
+          <p>
+            With no visible card number on its surface, Atlas keeps your privacy where it belongs,
+            in your own hands.⁷
+          </p>
+        </section>
+        <section class="writeblock">
+          <aside>Personalized for you</aside>
+          <p>
+            Designed front to back, Atlas card features tone-on-tone graphics and a personal icon of
+            your choice by your name.
+          </p>
+        </section>
+      </div>
+      <section class="powerCouple">
+        <h2>Power Couple</h2>
+        <h3>A dynamic duo of card + app.</h3>
+        <article>
+          Each conceived from the same first principles, Atlas shines with a true native app that
+          raises the bar, from magical rewards to unlimited cards, and everything in between.
+        </article>
+      </section>
+      <div class="phoneInHand">
+        <img src="../assets/images/phone-in-hand-desktop.webp" />
+      </div>
+      <p class="bespoke fronttoback">FAR ABOVE THE TYPICAL APP EXPERIENCE.</p>
+      <section class="writeblock">
+        <aside>Dynamic rewards⁸</aside>
+        <p>
+          Get automatically upgraded to 3x points where you spend the most, plus 1x points on every
+          purchase. Because there's more to a rewarding life than chasing points.
+        </p>
+      </section>
+      <section class="writeblock">
+        <aside>Unlimited virtual cards⁹</aside>
+        <p>
+          Create a unique card for your next trip, or one to share with your assistant, or to go
+          online shopping, or ... you get the idea.
+        </p>
+      </section>
+      <section class="writeblock">
+        <aside>Intuitively powerful</aside>
+        <p>
+          Buttery-smooth performance, support for features like Dark Mode, and endless details make
+          an app that's a pleasure to use, day in, night out.
+        </p>
+      </section>
+      <section class="threeimages">
+        <div class="moment">
+          <img src="../assets/images/moment-desktop.jpg" alt="" />
+          <span>YOUR PURCHASES, REIMAGINED AS MOMENTS.</span>
+        </div>
+        <div class="imageFlex">
+          <div class="virtualcards">
+            <img src="../assets/images/virtual-cards-desktop.jpg" alt="" />
+            <span>CREATE AND MANAGE VIRTUAL CARDS.</span>
+          </div>
+          <div class="rewards">
+            <img src="../assets/images/rewards-desktop.jpg" alt="" />
+            <span>POINTS UPGRADES, WITH DYNAMIC REWARDS.</span>
+          </div>
+        </div>
+      </section>
+      <section class="truly">
+        <h2>Truly Personal</h2>
+        <h3>At your beck and call (or text).</h3>
+        <article>
+          Experience handcrafted support, with your own dedicated personal advisor who is on hand to
+          fulfill any request, from last-minute dinner plans to deep dive sessions.
+        </article>
+      </section>
+      <div class="supportChat">
+        <img src="../assets/images/support-chat-desktop.jpg" alt="" />
+      </div>
+      <p class="bespoke">YOUR PERSONAL ADVISOR, NOW IN IMESSAGE.</p>
+      <div class="more">
+        <section class="writeblock">
+          <aside>Personal advisor</aside>
+          <p>
+            Going to exceptional lengths to know your preferences and tastes, your advisor is here
+            to provide truly personal support.
+          </p>
+        </section>
+        <section class="writeblock">
+          <aside>1 on 1 sessions</aside>
+          <p>
+            For those times when it takes more than a text or a phone call, your personal advisor is
+            just a quick booking away for a video call.
+          </p>
+        </section>
+        <section class="writeblock last">
+          <aside>Experiences concierge¹⁰</aside>
+          <p>
+            From last-minute table reservations, to sold out events, your personal advisor is on
+            hand to help you have a lifetime experience.
+          </p>
+        </section>
+      </div>
+      <div class="walletImg">
+        <img src="../assets/images/wallet-desktop.jpg" alt="" />
+        <p class="bespoke world">YOUR COMPANION THROUGH EVERY ADVENTURE.</p>
+      </div>
+    </div>
+    <div class="blackSection2">
+      <h2>Executive, brief.</h2>
+      <h3>Your next journey begins here.</h3>
+      <article>
+        With all the essentials you need out in the world, and nothing to limit your possibilities,
+        here is everything Atlas has to offer.
+      </article>
+      <section class="moreDetails">
+        <div>
+          <h3>Dining</h3>
+          <ol>
+            <li>Exclusive reservations with Atlas Supper Club</li>
+            <li>Dedicated tables at partner restaurants</li>
+            <li>Last-minute bookings</li>
+            <li>Peak hours access</li>
+          </ol>
+        </div>
+        <div>
+          <h3>Travel</h3>
+          <ol>
+            <li>5x points with BLADE on helicopter rides and flights</li>
+            <li>CLEAR Plus membership</li>
+            <li>Cancel flights without fees</li>
+          </ol>
+        </div>
+        <div>
+          <h3>Rewards & Benefits</h3>
+          <ol>
+            <li>3x upgrade on top spending category</li>
+            <li>1x points on every purchase</li>
+            <li>One Medical membership¹¹</li>
+            <li>Erewhon Cafe membership¹²</li>
+            <li>FUTURE fitness training membership¹³</li>
+          </ol>
+        </div>
+        <div>
+          <h3>Personal Advisor</h3>
+          <ol>
+            <li>Advisor dedicated to your account</li>
+            <li>1 on 1 sessions with available video calls</li>
+            <li>Personalized dining and experiences concierge</li>
+          </ol>
+        </div>
+        <div>
+          <h3>Card</h3>
+          <ol>
+            <li>Charge card with no pre-set spending limits¹⁴</li>
+            <li>21 gram steel alloy, milled, engraved, and polished to a brilliant mirror shine</li>
+            <li>Tone-on-tone card back, with your personal icon, and no card number</li>
+            <li>Visa Infinite</li>
+            <li>Handle with care, clean with a microfiber cloth</li>
+          </ol>
+        </div>
+        <div>
+          <h3>App</h3>
+          <ol>
+            <li>True native iOS app with buttery-smooth performance</li>
+            <li>Dark Mode support</li>
+            <li>Instant purchase notifications</li>
+            <li>Unlimited virtual cards</li>
+            <li>Apple Pay</li>
+          </ol>
+        </div>
+      </section>
+      <section class="founding">
+        <button>Become a founding member</button>
+        <p>$999/yr.</p>
+      </section>
+    </div>
+  </main>
+  <div class="links">
+    <ul>
+      <h3>ATLAS</h3>
+      <a href="">DINING</a>
+      <a href="">TRAVEL</a>
+      <a href="">CARD</a>
+      <a href="">APP</a>
+      <a href="">CONCIERGE</a>
+    </ul>
+    <ul>
+      <h3>BENEFITS</h3>
+      <a href="">ATLAS BENEFITS</a>
+      <a href="">VISA INFINITE</a>
+    </ul>
+    <ul>
+      <h3>CONTACT</h3>
+      <a href="mailto:">EMAIL</a>
+      <a href="">TWITTER</a>
+      <a href="">INSTAGRAM</a>
+    </ul>
+    <ul>
+      <h3>LEGAL</h3>
+      <a href="">PRIVACY POLICY</a>
+      <a href="">TERMS OF USE</a>
+      <a href="">STATEMENT DISCLOSURE</a>
+    </ul>
+    <ul>
+      <h3>ABOUT</h3>
+      <a href="">MEDIA KIT</a>
+    </ul>
+    <div>
+      <img src="../assets/images/bluelogo.png" alt="" />
+    </div>
+  </div>
+  <footer>
+    <div>
+      <ol>
+        <li>
+          ATLAS SUPPER CLUB RESERVATIONS ARE AVAILABLE ON A FIRST COME, FIRST SERVE BASIS
+          EXCLUSIVELY TO ATLAS CARDHOLDERS FROM WITHIN ATLAS APP. THE ATLAS SUPPER CLUB CODE OF
+          CONDUCT <a href="">(ATLASCARD.COM/CODE-OF-CONDUCT)</a> AND THE CANCELLATION POLICY OF THE
+          APPLICABLE RESTAURANT APPLY FOR EACH RESERVATION. LISTED RESTAURANTS ARE SUBJECT TO CHANGE
+          AT ANY TIME WITHOUT PRIOR NOTICE.
+        </li>
+        <li>LAST-MINUTE RESERVATIONS SUBJECT TO AVAILABILITY.</li>
+        <li>
+          5X POINTS WITH BLADE APPLIES TO ALL ELIGIBLE BLADE SERVICES. 5X POINTS IS CALCULATED AND
+          APPLIED AT THE END OF STATEMENT PERIODS THAT INCLUDE SETTLED BLADE TRANSACTIONS.
+        </li>
+        <li>
+          PROVIDED AS A $189 STATEMENT CREDIT TO YOUR ATLAS ACCOUNT UPON PURCHASE OF CLEAR PLUS
+          ANNUAL MEMBERSHIP WITH YOUR ATLAS CARD. VISIT <a href="">ENROLL.CLEARME.COM/ENROLL/</a> TO
+          LEARN MORE.
+        </li>
+        <li>
+          ATLAS CARDHOLDERS ARE ELIGIBLE TO RECEIVE REIMBURSEMENT FOR UP TO $600 PER YEAR OF
+          CANCELLATION/CHANGE FEE INCURRED WITH RESPECT TO ELIGIBLE AIRFARE PURCHASES THAT ARE
+          CANCELED FOR ANY REASON, FOR WHICH REIMBURSEMENT IS SOUGHT BEFORE THE BENEFIT END DATE,
+          AND IF THE ELIGIBLE CARDHOLDER IS NOT ELIGIBLE TO RECEIVE A REIMBURSEMENT FROM ANOTHER
+          SOURCE.
+        </li>
+        <li>VISIT <a href="">HTTPS://ATLASCARD.COM/VISA-INFINITE</a> TO LEARN MORE.</li>
+        <li>YOUR PHYSICAL CARD DETAILS ARE AVAILABLE TO ACCESS ANY TIME WITHIN ATLAS APP.</li>
+        <li>
+          REWARDED CATEGORIES ELIGIBLE FOR 3X POINTS UPGRADE INCLUDE DINING, FLIGHTS, HOTELS,
+          RIDESHARES, NIGHTLIFE, FASHION, GROCERIES, WELLNESS, HEALTH, HOME, AND AUTO SERVICES. 3X
+          POINTS UPGRADE IS CALCULATED AND APPLIED AT THE END OF EACH STATEMENT PERIOD ON SETTLED
+          TRANSACTIONS WITHIN YOUR TOP REWARDED CATEGORY BY DOLLAR AMOUNT. OUTSIDE OF THE TOP SPEND
+          CATEGORY, SETTLED TRANSACTIONS FOR PRODUCTS AND SERVICES PROVIDED BY MERCHANTS AND/OR
+          CATEGORIES NOT SUBJECT TO ANOTHER POINTS MULTIPLIER, WILL ACCRUE 1 POINT FOR EVERY DOLLAR
+          SPENT (1X POINTS) AFTER THE APPLICABLE BILLING CYCLE. SUBJECT TO RESTRICTIONS AND THE
+          REWARDS & BENEFIT TERMS.
+        </li>
+        <li>
+          CREATE UNLIMITED NEW CARDS WITHIN ATLAS APP, WITH UP TO FIVE ACTIVE CARDS AT ANY ONE TIME.
+        </li>
+        <li>
+          CARDHOLDER IS RESPONSIBLE FOR THE COST OF PURCHASES OF GOODS AND SERVICES AND/OR THIRD
+          PARTY EXPENSES AND SERVICES INCURRED IN RELATION TO ANY FULFILLED REQUESTS.
+        </li>
+        <li>
+          PROVIDED AS A $199 STATEMENT CREDIT TO YOUR ATLAS ACCOUNT UPON PURCHASE OF ONE MEDICAL
+          ANNUAL MEMBERSHIP WITH YOUR ATLAS CARD. VISIT <a href="">ONEMEDICAL.COM/ATLAS/</a> TO
+          LEARN MORE.
+        </li>
+        <li>
+          COMPLIMENTARY $100 ANNUAL EREWHON CAFE MEMBERSHIP. BENEFIT ENROLLMENT INSTRUCTIONS
+          PROVIDED IN APP.
+        </li>
+        <li>
+          UP TO $300 IN ANNUAL STATEMENT CREDITS ($25/MONTH). GET YOUR FIRST MONTH FREE BY USING THE
+          FUTURE FITNESS ATLAS CARDHOLDER ENROLLMENT PAGE. VISIT
+          <a href="">FUTURE.CO/ATLASCARD</a> TO LEARN MORE.
+        </li>
+        <li>
+          ATLAS IS A TRUE CHARGE CARD WITH NO PRE-SET SPENDING LIMITS. THIS MEANS YOUR SPENDING
+          LIMIT IS FLEXIBLE. UNLIKE TRADITIONAL CARDS WITH SET LIMITS, THE AMOUNT YOU CAN SPEND
+          FLEXIBLY ADAPTS BASED ON A RANGE OF FACTORS SUCH AS YOUR PURCHASES, STATEMENT BALANCE
+          PAYMENTS, AND CREDIT HISTORY.
+        </li>
+      </ol>
+    </div>
+  </footer>
+  <div class="insurance">
+    <p>ISSUED BY LEAD BANK | 2023 © ATLAS EXPLORATION, INC.</p>
+    <p>
+      ATLAS EXPLORATION, INC IS A TECHNOLOGY COMPANY, NOT A BANK. ATLAS CARD IS ISSUED BY LEAD BANK
+      ("ISSUER"). AN APPLICATION IS REQUIRED. CARDHOLDERS MUST BE AT LEAST 18 YEARS OLD AND A
+      CITIZEN OF THE UNITED STATES, OR A PERMANENT RESIDENT WITH A VALID SOCIAL SECURITY NUMBER
+      (SSN) OR ITIN. FINANCING IS SUBJECT TO ISSUER'S APPROVAL. PAYMENT OF ACCOUNT BALANCE DUE IN
+      FULL MONTHLY. FEES MAY BE ASSESSED ON OVERDUE AMOUNTS. AN ANNUAL MEMBERSHIP FEE APPLIES.
+    </p>
+  </div>
+</template>
